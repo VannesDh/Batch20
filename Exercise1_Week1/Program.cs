6da013bda2efe6e18702ed929725c231caae3a57
VannesDh <vannesdharmawann@gmail.com>
2026-08-03 14:13:17 +0700

    Initial Commit

---
+++ Exercise1_Week1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+namespace Program;
+
+class Program
+{
+    static void Main()
+    {
+       Console.WriteLine(Generate(15));
+    }
+
+    public static string Generate(int n)
+    {
+        StringBuilder stringBuilder = new();
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0)
+            {
+                stringBuilder.Append("foobar");
+            }
+            else if(i % 3 == 0)
+            {
+                stringBuilder.Append("foo");
+            }
+            else if(i % 5 == 0)
+            {
+                stringBuilder.Append("bar");
+            }
+            else
+            {
+                stringBuilder.Append(i);
+            }
+            
+            if(i != n)
+            {
+                stringBuilder.Append(", ");
+            }
+        }
+        return stringBuilder.ToString();
+    }
+}
+
+
+
+
+
+
