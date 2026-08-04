4778656f6bfb2695e30227ef814664f71ccc1a31
VannesDh <vannesdharmawann@gmail.com>
2026-08-04 08:52:56 +0700

    Initial Commit For Exercise 2 Week 1

---
+++ Exercise2_Week1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace Exercise2_Week1;
+
+class Exercise2
+{
+    static void Main()
+    {
+        QueueProcess queueProcess = new();
+        queueProcess.Enqueue("A");
+        queueProcess.Enqueue("B");
+        queueProcess.Process();
+        queueProcess.Process();
+        queueProcess.Process();
+    }
+}
+
+class QueueProcess()
+{
+    private Queue _queue = new();
+
+    public void Enqueue(object obj)
+    {
+        Console.WriteLine("Queued {0}",obj);
+        _queue.Enqueue(obj);
+    }
+
+    public void Process()
+    {
+        if(_queue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Console.WriteLine("Processed {0}", _queue.Peek());
+            _queue.Dequeue();
+        }
+    }
+}
+
